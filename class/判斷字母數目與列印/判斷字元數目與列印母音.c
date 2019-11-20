a88393f162c5cfa55dc9b4bb84b03ba5997d6e7e
Iris-Lien <jolinqq918@gmail.com>
2019-11-20 23:18:52 +0800

    done

--- class/判斷字母數目與列印/判斷字元數目與列印母音.c
+++ class/判斷字母數目與列印/判斷字元數目與列印母音.c
@@ -4,18 +4,18 @@
 
 int main ( void )
 {
-    char input[100];
-    char string[100];//ダ
+    char input[100]={0};
+    char string[100]={0};//ダ
     int number=0,alpha=0;//璸计Ω计,ダΩ计
-    int i;
+    int i,j;
     int a=0,b=0;//璸ダΩ计Ω计
 
-    scanf("%s",input);
-    printf ("%s\n",input);
+    scanf("%[^\n]",input);
+    //printf ("%s\n",input);
     for(i=0;input[i]!='#'&&input[i]!='\0';i++)
     {
-        printf("%c",input[i]);
-        printf("\n");
+        //printf("%c",input[i]);
+        //printf("\n");
         if(isdigit(input[i]))
             {number++;}
         if(isalpha(input[i]))
@@ -24,13 +24,16 @@ int main ( void )
             if(input[i]=='a'||input[i]=='e'||input[i]=='i'||input[i]=='o'||input[i]=='u'
                ||input[i]=='A'||input[i]=='E'||input[i]=='I'||input[i]=='O'||input[i]=='U')
             {
+                string[a]=input[i];
                 a++;
-
             }
             else
                 {b++;}
         }
     }
     printf("%d\n%d\n%d\n%d\n",number,alpha,a,b);
+    for(j=0;j<a;j++)
+        printf("%c",string[j]);
 
+    return 0;
 }
