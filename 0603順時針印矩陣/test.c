7bcea69a957398fde9eb9d7f8e022c09d53a8996
Iris-Lien <jolinqq918@gmail.com>
2019-12-04 21:43:30 +0800

    done

--- 0603順時針印矩陣/test.c
+++ 0603順時針印矩陣/test.c
@@ -7,19 +7,20 @@ main()
     int m,n;
     int i,j;
 
-    scanf("%d%d",&n,&m);
-    int array[n][m];
+    scanf("%d%d",&m,&n);
+    int array[m][n];
 
-    for(i=0;i<n;i++)
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<m;j++)
+        for(j=0;j<n;j++)
         {
             scanf("%d",&(array[i][j]));
         }
     }
-     while ( (round < (m / 2) + (m % 2)) && (round < (n / 2) + (n % 2)) ) // 當索引值超過陣列的一半大小 表示已經印完整個陣列（因為是繞圈印出）
+     while ( (round < (m / 2) + (m % 2)) && (round < (n / 2) + (n % 2)) )
      {
         i = j = round;
+
         for (; j < n - round - 1; j++)
           printf("%d ", array[i][j]);
 
@@ -32,7 +33,7 @@ main()
         for (; round < i; i--)
           printf("%d ", array[i][j]);
 
-        if (((m - round * 2) == 1) || ((n - round * 2) == 1)) // 若是矩陣已經縮成 1 x 1 則印出唯一元素（前面的判斷條件無法印出這種狀況）
+        if (((m - round * 2) == 1) && ((n - round * 2) == 1))
           printf("%d ", array[i][j]);
 
         round++;
