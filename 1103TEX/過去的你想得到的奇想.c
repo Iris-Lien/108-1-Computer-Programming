b9ba90b4802facb7768811011502685286cd9d79
Iris-Lien <56546170+Iris-Lien@users.noreply.github.com>
2020-01-04 22:02:57 +0800

    first commit->done

--- 1103TEX/過去的你想得到的奇想.c
+++ 1103TEX/過去的你想得到的奇想.c
@@ -1,33 +1,40 @@
+/*
+input ex:
+"To be or not to be," quoth the Bard, "that
+is the question".
+The programming contestant replied: "I must disagree.
+To `C' or not to `C', that is The Question!"
+
+output ex:
+``To be or not to be,'' quoth the Bard, ``that
+is the question''.
+The programming contestant replied: ``I must disagree.
+To `C' or not to `C', that is The Question!''
+*/
+
 #include <stdio.h>
 
 int main()
 {
     char input, cond;
     int counter = 0;
-
-    for (
-        cond  = scanf("%c", &input);
-        cond != EOF;
-        cond  = scanf("%c", &input)
-    )
+    while(scanf("%c", &input)!= EOF)
     {
         if (input == '"')
         {
             switch (counter % 2)
             {
-            case 0:
-                printf("``");
-                break;
-            case 1:
-                printf("''");
-                break;
+                case 0:
+                    printf("``");
+                    break;
+                case 1:
+                    printf("''");
+                    break;
             }
-
             counter++;
         }
         else
             printf("%c", input);
     }
-
     return 0;
 }
