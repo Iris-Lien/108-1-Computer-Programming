e200e6a462495a64c87ff6ac7efe327ab7e4c119
Iris-Lien <56546170+Iris-Lien@users.noreply.github.com>
2020-01-04 22:08:24 +0800

    first commit->done

--- 0601稀疏矩陣/0601稀疏矩陣.c
+++ 0601稀疏矩陣/0601稀疏矩陣.c
@@ -1,3 +1,11 @@
+/*
+iutput ex:          output ex:
+5 6 4               0 0 0 0 0 0 
+1 1 3               0 3 0 0 0 0 
+2 3 6               0 0 0 6 0 0 
+3 2 9               0 0 9 0 0 0 
+4 4 12              0 0 0 0 12 0 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,7 +36,5 @@ int main()
         }
         printf("\n");
     }
-
-
     return 0;
 }
